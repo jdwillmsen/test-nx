f2a535c4271fdbb5b6269a3c56010a3260d0d27b
Jake Willmsen <jdwillmsen@gmail.com>
2023-10-19 21:54:35 -0500

    Formatting fix

--- apps/test/src/app/app.component.ts
+++ apps/test/src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
-import {TestLibOneComponent} from "@org/test-lib-one";
+import { TestLibOneComponent } from '@org/test-lib-one';
 
 @Component({
   standalone: true,
-    imports: [NxWelcomeComponent, RouterModule, TestLibOneComponent],
+  imports: [NxWelcomeComponent, RouterModule, TestLibOneComponent],
   selector: 'org-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
