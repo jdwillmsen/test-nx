a3ed5fee3904204d029f0457ed09709abec5a782
Jake Willmsen <jdwillmsen@gmail.com>
2023-10-22 15:57:04 -0500

    Formatting fix

--- apps/test/src/app/nx-welcome.component.cy.ts
+++ apps/test/src/app/nx-welcome.component.cy.ts
@@ -1,6 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 import { NxWelcomeComponent } from './nx-welcome.component';
-import {mount} from "cypress/angular";
+import { mount } from 'cypress/angular';
 
 describe(NxWelcomeComponent.name, () => {
   beforeEach(() => {
@@ -19,5 +19,5 @@ describe(NxWelcomeComponent.name, () => {
   it('header is setup correctly', () => {
     mount(NxWelcomeComponent);
     cy.get('h1').should('contain.text', 'Hello there,');
-  })
+  });
 });
