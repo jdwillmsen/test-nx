88368886d9d23f8ba45a8824018164b1df540737
Jake Willmsen <jdwillmsen@gmail.com>
2023-10-26 20:43:58 -0500

    Test CI

--- libs/test-lib-two/src/lib/component-one/component-one.component.cy.ts
+++ libs/test-lib-two/src/lib/component-one/component-one.component.cy.ts
@@ -2,18 +2,16 @@ import { TestBed } from '@angular/core/testing';
 import { ComponentOneComponent } from './component-one.component';
 
 describe(ComponentOneComponent.name, () => {
-
   beforeEach(() => {
     TestBed.overrideComponent(ComponentOneComponent, {
       add: {
         imports: [],
-        providers: []
-      }
-    })
-  })
+        providers: [],
+      },
+    });
+  });
 
   it('renders', () => {
-     cy.mount(ComponentOneComponent,);
-  })
-
-})
+    cy.mount(ComponentOneComponent);
+  });
+});
